19247875381e509ea8bd2a65f6adffd335e8ee82
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-03-11 14:24:38 -0500

    Line 24, fix in string incorrect

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 import uuid
-import datetime
+import datetime import datetime
 """
 Module that defines all common attributes/methods for other classes
 """
@@ -8,18 +8,18 @@ Module that defines all common attributes/methods for other classes
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         obj_dictionary = self.__dict__.copy()
         obj_dictionary["__class__"] = self.__class__.__name__
         obj_dictionary["created_at"] = self.created_at.isoformat()
-        obj_dictionary["update_at"] = self.updated_at.isoformat()
+        obj_dictionary["updated_at"] = self.updated_at.isoformat()
         return obj_dictionary
