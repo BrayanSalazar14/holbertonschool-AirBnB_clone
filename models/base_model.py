7a3c9b4a50e9b4b8c82fe39a11d0d3400ca3bf47
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-03-11 16:12:29 -0500

    task 4 project airbnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,19 @@ Module that defines all common attributes/methods for other classes
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            return
+        for key, value in kwargs.items():
+            if key != "__class__":
+                setattr(self, key, value)
+            if key == "created_at":
+                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            if key == "updated_at":
+                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
