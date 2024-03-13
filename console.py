b8bdbb7dc408d7c00e32273529658ce0a263eb9c
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-03-13 00:18:58 -0500

    hot fix

--- console.py
+++ console.py
@@ -7,15 +7,19 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Simple command processor example. """
-    prompt = "(hbnb) "
+    """ HBNH console """
+    prompt = '(hbnb) '
+
+    def do_EOF(self, arg):
+        """Exits console"""
+        return True
+
+    def emptyline(self):
+        """ overwriting the emptyline method """
+        return False
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
-        return True
-
-    def do_EOF(self, arg):
-        """Exit the console"""
         return True
 
 
