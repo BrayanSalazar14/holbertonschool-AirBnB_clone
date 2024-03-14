aefe8715f7ed0a57cd854163ae6e529ad3484bc9
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-03-14 18:46:27 -0500

    hot fix

--- models/place.py
+++ models/place.py
@@ -14,6 +14,6 @@ class Place(BaseModel):
     number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
-    price_by_night = float(0)
-    longitude = float(0)
+    price_by_night = 0.0
+    longitude = 0.0
     amenity_ids = []
