6d7a2656a6c2a1dc7a0e2e1a3b586bc665cf4a87
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-03-14 18:55:23 -0500

    Hot fix

--- models/place.py
+++ models/place.py
@@ -6,15 +6,14 @@ Module in the Place
 
 
 class Place(BaseModel):
-    def __init__(self):
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0
-        longitude = 0.0
-        amenity_ids = []
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0
+    longitude = 0.0
+    amenity_ids = []
