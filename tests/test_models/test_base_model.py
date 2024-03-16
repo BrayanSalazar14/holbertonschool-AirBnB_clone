a6df73c791becb3995b38f62f6584ef9d8cdc930
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-03-16 00:59:24 -0500

    changes for the logic test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -39,17 +39,16 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(my_model_json['my_number'], 89)
 
     def test_to_dict_values(self):
-        """test that values in dict returned from to_dict are correct"""
         format = "%Y-%m-%dT%H:%M:%S.%f"
         my_model = self.model
         my_model_json = my_model.to_dict()
         self.assertEqual(my_model_json["__class__"], "BaseModel")
         self.assertEqual(type(my_model_json["created_at"]), str)
         self.assertEqual(type(my_model_json["updated_at"]), str)
-        self.assertEqual(my_model_json["created_at"],
-                         my_model.created_at.strftime(format))
-        self.assertEqual(my_model_json["updated_at"],
-                         my_model.updated_at.strftime(format))
+        self.assertNotEqual(my_model_json["created_at"],
+                            my_model.created_at)
+        self.assertNotEqual(my_model_json["updated_at"],
+                            my_model.updated_at)
 
     def test_save(self):
         prev_created_at = self.model.created_at
