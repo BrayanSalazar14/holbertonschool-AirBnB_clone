0b1b6b165489483d6418ca316179ea1534d58b6c
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-03-16 13:11:42 -0500

    test new

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -18,3 +18,10 @@ class TestFileStorage(unittest.TestCase):
         new_dict = self.model.all()
         self.assertEqual(type(new_dict), dict)
         self.assertIs(new_dict, self.model._FileStorage__objects)
+
+    def test_new(self):
+        my_model = BaseModel()
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
+        key = f"{my_model.__class__.__name__}.{my_model.id}"
+        self.assertIn(key, self.model._FileStorage__objects, "No exists")
