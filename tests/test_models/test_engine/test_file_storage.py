82dbcd09220f22dd15e8b061d28335110bf1fe8d
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-03-16 13:35:06 -0500

    test save

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import unittest
+import json
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+
+
+classes = {"BaseModel": BaseModel}
 
 
 class TestFileStorage(unittest.TestCase):
@@ -25,3 +29,16 @@ class TestFileStorage(unittest.TestCase):
         my_model.my_number = 89
         key = f"{my_model.__class__.__name__}.{my_model.id}"
         self.assertIn(key, self.model._FileStorage__objects, "No exists")
+
+    def test_save(self):
+        objs = {}
+        save = FileStorage._FileStorage__objects
+        FileStorage._FileStorage__objects = objs
+        self.model.save()
+        FileStorage._FileStorage__objects = save
+        for key, value in objs.items():
+            objs[key] = value.to_dict()
+        file_json = json.dumps(objs)
+        with open("file.json", "r") as file:
+            file_read = file.read()
+        self.assertEqual(json.loads(file_json), json.loads(file_read))
