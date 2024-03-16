05879d1c81f80fae3e5dd9b4a9662b6eaa716407
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-03-16 17:16:55 -0500

    test Review

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,54 @@
+import unittest
+from models.base_model import BaseModel
+from models.review import Review
+
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.model = Review()
+
+    def tearDown(self):
+        del self.model
+
+    def daughterClass(self):
+        self.assertIsInstance(self.model, BaseModel)
+        self.assertTrue(hasattr(self.model, "id"))
+        self.assertTrue(hasattr(self.model, "created_at"))
+        self.assertTrue(hasattr(self.model, "updated_at"))
+
+    def test_atrr(self):
+        my_review = self.model
+        self.assertTrue(hasattr(my_review, "place_id"))
+        self.assertTrue(hasattr(my_review, "user_id"))
+        self.assertTrue(hasattr(my_review, "text"))
+
+    def test_to_dict(self):
+        my_review = self.model
+        my_review.place_id = "***a"
+        my_review.user_id = "*****123"
+        my_review.text = "Brayan"
+        my_model_json = my_review.to_dict()
+        attrs = ["id",
+                 "created_at",
+                 "updated_at",
+                 "place_id",
+                 "user_id",
+                 "text",
+                 "__class__"]
+        self.assertCountEqual(my_model_json.keys(), attrs)
+        self.assertEqual(my_model_json['__class__'], 'Review')
+        self.assertEqual(my_model_json['place_id'], "***a")
+        self.assertEqual(my_model_json['user_id'], "*****123")
+        self.assertEqual(my_model_json['text'], "Brayan")
+
+    def test_to_dict_values(self):
+        format = "%Y-%m-%dT%H:%M:%S.%f"
+        my_model = self.model
+        my_model_json = my_model.to_dict()
+        self.assertEqual(my_model_json["__class__"], "Review")
+        self.assertEqual(type(my_model_json["created_at"]), str)
+        self.assertEqual(type(my_model_json["updated_at"]), str)
+        self.assertNotEqual(my_model_json["created_at"],
+                            my_model.created_at)
+        self.assertNotEqual(my_model_json["updated_at"],
+                            my_model.updated_at)
